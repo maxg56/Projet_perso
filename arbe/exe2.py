190aa851a8920594dd7ba102c0b60774c7b3885a
maxg56 <mgendrot56@gmail.com>
2023-12-19 20:22:26 +0100

    madkfk

--- arbe/exe2.py
+++ arbe/exe2.py
@@ -1,14 +1,31 @@
-
-from arbe.pilesFiles import Pile, File
+from pilesFiles import Pile, File
 from Arbres import Arbre
 
-ardre =("*",[
+arbre =("*",[
     ("+",[(3,[]),(8,[])]),
     ("-",[(5,[]),(6,[])])
     ])
 '("",[])'
 ["x",[]]
-def convrticere(forme,ardre) -> Arbre : 
+def convrticere(forme,ardre)  : 
+    Valeur ,enfant = arbre
 
+    if len(arbre) == 0:
+        return Arbre()
+    if arbre[1] == []:
+        return Arbre(arbre[0])
+    voai = []
+    for enfant in ardre[1]:
+        voai.append( convrticere(enfant))
+    arbre = Arbre(arbre[0],voai)
+    return arbre
 
+def parcourir_arbre(arbre):
+   Valeur ,enfant = arbre
 
+    print(symbole)  # Fais quelque chose avec le symbole, par exemple l'afficher
+
+    for sous_arbre in sous_arbres:
+        parcourir_arbre(sous_arbre)
+
+parcourir_arbre(arbre)
