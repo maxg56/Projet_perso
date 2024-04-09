8548a7f6091ba633f1a9aeb2980a6d6b74d57688
maxg56 <mgendrot56@gmail.com>
2024-04-09 09:01:47 +0200

    hanoi fosenle

--- pyhton/hanoi/hanoi_1.py
+++ pyhton/hanoi/hanoi_1.py
@@ -13,7 +13,7 @@ class Bloc:
     """Classe représentant un bloc dans la tour de Hanoi."""
     def __init__(self, valeur :int , longueur:int, hauteur:int, x:int, y:int) -> None:
         self.valeur = valeur
-        self.longueur = longueur
+        self.longueur = int(longueur)
         self.hauteur = hauteur
         self.x, self.y = x, y
         
@@ -36,25 +36,33 @@ class Toure :
     """Classe représentant une tour dans le jeu de Hanoi."""
     def __init__(self, x_base :int ,y_base:int =Y_BASE ,etage :int = None ) -> None:
         self.tours = Piles()
-        self.ref_x = x_base +1
+        self.ref_x = x_base 
         self.ref_y = y_base -8
         self.r =6
         if etage is not None:
             rec_height = 8
             
             largeur_etage_min = 11
-            largeur_etage = largeur_etage_min +6*etage
+            largeur_etage = int(largeur_etage_min +6*etage)
             if largeur_etage > LARGEUR_BASE:
                 largeur_etage = LARGEUR_BASE
                 self.r =2.2
             
+            
+
             for n in range(etage,0,-1):
-                
                 largeur_etage  -=self.r
-                self.tours.empiler(Bloc(n, largeur_etage, rec_height,self.ref_x, self.ref_y))
+                if self.tours.est_vide():
+                    b_ref_x =self.ref_x + LARGEUR_BASE//2 - largeur_etage//2
+                else:
+                    b2 :Bloc = self.tours.depiler()
+                    b_ref_x = b2.x +b2.longueur//2 - largeur_etage//2
+                    self.tours.empiler(b2)
+                x2 = b_ref_x 
+                self.tours.empiler(Bloc(n, largeur_etage, rec_height,b_ref_x, self.ref_y))
                 self.ref_y -= rec_height
-                self.ref_x +=self.r/2
-            print(self.tours)
+                
+            
             
         
     def dessine_tore(self) -> Piles:
@@ -71,39 +79,45 @@ class Toure :
     def deplasé_vre(self,toure ) :
         if type(toure) is Toure and not self.tours.est_vide():
             b :Bloc = self.tours.depiler()
-            print(b)
-            b.Déplace(toure.ref_x,toure.ref_y)
+            if not toure.tours.est_vide():
+                b2 :Bloc = toure.tours.depiler()
+                ref_x = b2.x + b2.longueur//2 -b.longueur//2
+                toure.tours.empiler(b2)
+            else:
+                 ref_x = toure.ref_x  +LARGEUR_BASE//2- b.longueur//2
+            b.Déplace(ref_x,toure.ref_y)
             toure.tours.empiler(b)
-            toure.ref_x += self.r/2
+            self.ref_y += 8
             toure.ref_y -= 8
 
    
 class DisplayHanoi:
     """Classe principale pour afficher le jeu de Hanoi."""
-    def __init__(self, nb_disques,pause_t,):
-        # Initialisation de Pyxel avec une résolution de 160x120 pixels et une fréquence d'images de 30 par seconde
-        pyxel.init(LARGEUR, HAUTEUR, fps=30)
+    def __init__(self, nb_disques :int ,fps :int =30,ot :bool = True, __x1__  = X_BASE_1,__X2__ =X_BASE_2,__X3__ =X_BASE_3) ->None:
         
+        pyxel.init(LARGEUR, HAUTEUR, fps=fps)
+        self.ot = True
         self.nb_disques = nb_disques
-        self.tour_debut= Toure(X_BASE_1,Y_BASE,nb_disques)
-        self.tour_milue = Toure(X_BASE_2)
-        self.tour_fin = Toure(X_BASE_3)
-        self.frame_count = 0
-        self.pause_time = pause_t
-        self.orde=self.sousion()
+        self.tour_debut= Toure(__x1__,Y_BASE,nb_disques)
+        self.tour_milue = Toure(__X2__)
+        self.tour_fin = Toure(__X3__)
+        
+        self.i_orde = 0
+        self.frame_count =0
+        if self.ot :
+            self.orde=self.sousion()
         
         # Exécution de la boucle principale du jeu avec les fonctions update et draw
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        # Incrémentation du compteur de frames
         self.frame_count += 1
-        ide = 0
-        if self.frame_count == 60:
-            if ide <= len(self.orde):
-                self.sd(self.orde[ide],self.tour_debut,self.tour_milue,self.tour_fin)
-                ide += 1
-            
+        
+        if self.frame_count == 30:
+
+            if self.i_orde < len(self.orde) and self.ot:
+                self.sd(self.orde[self.i_orde],self.tour_debut,self.tour_milue,self.tour_fin)
+                self.i_orde += 1
             self.frame_count = 0
             
 
@@ -165,4 +179,4 @@ class DisplayHanoi:
 
 
 
-a = DisplayHanoi(10,4)
+a = DisplayHanoi(10)
