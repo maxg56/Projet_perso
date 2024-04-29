3ccf87b13e67315441d862f4ea19e82011f5913b
maxg56 <mgendrot56@gmail.com>
2024-04-29 17:18:47 +0200

    lavi

--- pyhton/pygame/lavi/bruit_de_perlin.py
+++ pyhton/pygame/lavi/bruit_de_perlin.py
@@ -1,4 +1,4 @@
-import noise 
+from noise import pnoise2
 from PIL import Image
 import random
 taille = (128, 128)
@@ -23,30 +23,35 @@ def sec_color(x,y,image,value,):
     elif value < 1.0:
         image.putpixel((x, y), snow)
 
-def bruit_de_perlin(longueur,Auteur):
-    """Cette fonction utilise l'algorithme De bruit de perling puis ressort une matrice"""
-    matrice_de_pixel = [[None for _ in range(Auteur)]for _  in range(longueur)]
-    scale = round(random.uniform(10.0, 80.0),2)
+def bruit_de_perlin(longueur, hauteur):
+    """Cette fonction utilise l'algorithme de bruit de Perlin puis retourne une matrice"""
+
+    matrice_de_pixel = [[None for _ in range(hauteur)] for _ in range(longueur)]
+    scale = round(random.uniform(10.0, 80.0), 2)
     octaves = 6
-    lacunarity = round(random.uniform(1.5, 2.0),2)
-    persistence = round(random.uniform(0.5, 0.7),2)
-    print(f"scale : {scale} , lacunarity: {lacunarity} ,persistence :{persistence}")
+    lacunarity = round(random.uniform(1.5, 2.0), 2)
+    persistence = round(random.uniform(0.5, 0.7), 2)
+    
     for x in range(longueur):
-        for y in range(Auteur):
-            value = noise.pnoise2(x/scale,
-                                y/scale,
-                                octaves=octaves,
-                                persistence=persistence,
-                                lacunarity=lacunarity,
-                                repeatx=longueur,
-                                repeaty=Auteur,
-                                base=0
-                                )
+        for y in range(hauteur):
+            value = pnoise2(x / scale,
+                            y / scale,
+                            octaves=octaves,
+                            persistence=persistence,
+                            lacunarity=lacunarity,
+                            repeatx=longueur,
+                            repeaty=hauteur,
+                            base=0
+                            )
             matrice_de_pixel[x][y] = value
     return matrice_de_pixel
 
+
+
+
 if __name__ == "__main__":
-    taille = (1980, 1080)
+    taille = (120, 120)
+    Taille_case = 4
     for i in range(10):
         image_Nom = f"noise{i}.png"
         matrice = bruit_de_perlin(taille[0],taille[1])
