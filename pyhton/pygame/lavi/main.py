c0d5ee9b7a0b35f01e771144508e2d20234ef782
maxg56 <mgendrot56@gmail.com>
2024-04-30 15:27:23 +0200

    TEstris

--- pyhton/pygame/lavi/main.py
+++ pyhton/pygame/lavi/main.py
@@ -11,6 +11,7 @@ class Game:
         self.screen = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption("Mon jeu Pygame")
         self.image = pygame.image.load("/home/max_dev/Documents/git/Projet_perso/pyhton/pygame/lavi/ressources/tente.png")
+        self.image2 = pygame.image.load("/home/max_dev/Documents/git/Projet_perso/pyhton/pygame/lavi/ressources/peti.png")
         self.camera_x = 0
         self.camera_y = 0
 
@@ -26,9 +27,9 @@ class Game:
             self.camera_y += 5
 
     def render(self):
-        self.screen.fill((0, 255, 0))
+        self.screen.fill((100, 200, 30))
         self.screen.blit(self.image, (100 - self.camera_x, 100 - self.camera_y))
-        
+        self.screen.blit(self.image2, (10 - self.camera_x, 10 - self.camera_y))
         pygame.display.flip()
 
     def run(self):
